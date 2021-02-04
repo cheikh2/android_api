02c3de84b3bb9b00abdc21f2bb27bebb31b6023f
cheikh Mbow <cheikh2mbow@gmail.com>
2021-02-04 01:48:41 +0000

    troisieme commit

--- app/src/main/java/com/cm/gestionpharmacie/UI/FairEnvoi.java
+++ app/src/main/java/com/cm/gestionpharmacie/UI/FairEnvoi.java
@@ -12,12 +12,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.cm.gestionpharmacie.EnvoiAdapter;
 import com.cm.gestionpharmacie.R;
@@ -36,7 +38,7 @@ public class FairEnvoi extends AppCompatActivity {
     SwipeRefreshLayout refresh;
     EnvoiAdapter envoiAdapter;
     Dialog dialog;
-    String url="http://192.168.1.6:8080/envoie";
+    String urlAdd="http://192.168.42.12:8080/envoie";
 
     private EditText montant, prenomEme, nomEme, cni, telephoneEme, prenomRec, nomRec, telephoneRec;
     private Button saveButton;
@@ -72,74 +74,73 @@ public class FairEnvoi extends AppCompatActivity {
         final String myTelephoneEme = telephoneEme.getText().toString().trim();
         final String myPrenomRec = prenomRec.getText().toString().trim();
         final String myNomRec = nomRec.getText().toString().trim();
-        final String mytelephoneRec = telephoneRec.getText().toString().trim();
+        final String myTelephoneRec = telephoneRec.getText().toString().trim();
 
         if (TextUtils.isEmpty(myMontant)){
             montant.setError("Remplir le montant");
             montant.requestFocus();
             return;
         }
-        if (TextUtils.isEmpty(myMontant)){
+        if (TextUtils.isEmpty(myPrenomEme)){
             prenomEme.setError("Remplir le prenom");
             prenomEme.requestFocus();
             return;
         }
-        if (TextUtils.isEmpty(myMontant)){
+        if (TextUtils.isEmpty(myNomEme)){
             nomEme.setError("Remplir le nom");
             nomEme.requestFocus();
             return;
         }
-        if (TextUtils.isEmpty(myMontant)){
+        if (TextUtils.isEmpty(myTelephoneEme)){
             telephoneEme.setError("Remplir le telephone");
             telephoneEme.requestFocus();
             return;
         }
-        if (TextUtils.isEmpty(myMontant)){
+        if (TextUtils.isEmpty(myCni)){
             cni.setError("Remplir le cni");
             cni.requestFocus();
             return;
         }
-        if (TextUtils.isEmpty(myMontant)){
+        if (TextUtils.isEmpty(myPrenomRec)){
             prenomRec.setError("Remplir le prenom");
             prenomRec.requestFocus();
             return;
         }
-        if (TextUtils.isEmpty(myMontant)){
+        if (TextUtils.isEmpty(myNomRec)){
             nomRec.setError("Remplir le nom");
             nomRec.requestFocus();
             return;
         }
-        if (TextUtils.isEmpty(myMontant)){
+        if (TextUtils.isEmpty(myTelephoneRec)){
             telephoneRec.setError("Remplir le telephone");
             telephoneRec.requestFocus();
             return;
         }
 
+        JSONObject postparams = new JSONObject();
+        try {
+            postparams.put("montant",myMontant);
+            postparams.put("prenomEme",myPrenomEme);
+            postparams.put("nomEme",myNomEme);
+            postparams.put("telephoneEme",myTelephoneEme);
+            postparams.put("cni",myCni);
+            postparams.put("prenomRec",myPrenomRec);
+            postparams.put("nomRec",myNomRec);
+            postparams.put("telephoneRec",myTelephoneRec);
+        }catch (JSONException e){
+            e.getMessage();
+        }
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST,url, null, new Response.Listener<JSONArray>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,urlAdd, postparams, new Response.Listener<JSONObject>() {
             @Override
-            public void onResponse(JSONArray response) {
-                for (int i = 0; i < response.length(); i++) {
-                    try {
-                        JSONObject envoiObjet = response.getJSONObject(i);
-                        Envoi envoi = new Envoi();
-                        envoi.setId(envoiObjet.getInt("id"));
-                        envoi.setDateEnvoi(envoiObjet.getString("dateEnvoi"));
-                        envoi.setMontant(envoiObjet.getDouble("montant"));
-                        //JSONObject Jarray  = envoiObjet.getJSONObject("emetteur");
-                        //envoi.setEmetteur(Jarray.getJSONObject("id"));
-                        //System.out.println(Jarray);
-                        //envoi.setEmetteur(Jarray);
-                        //envoi.setRecepteur(envoiObjet.getJSONObject("recepteur"));
-                        /*JSONObject recepteurObjet = response.getJSONObject(i);
-                        Recepteur recepteur = new Recepteur();
-                        recepteur.setId(recepteurObjet.getInt("id"));*/
+            public void onResponse(JSONObject response) {
 
-                        envois.add(envoi);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                try {
+                    Toast.makeText(getApplicationContext(), "transfert reussi", Toast.LENGTH_LONG).show();
+                }catch (Exception e){
+                    e.printStackTrace();
+
                 }
                 recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
                 envoiAdapter = new EnvoiAdapter(getApplication(), envois);
@@ -151,6 +152,6 @@ public class FairEnvoi extends AppCompatActivity {
                 Log.d("tag", "onErroResponse: " + error.getMessage());
             }
         });
-        queue.add(jsonArrayRequest);
+        queue.add(jsonObjectRequest);
     }
 }
